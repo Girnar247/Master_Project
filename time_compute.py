68f533be398820cab6ae00f4f02ed6da0a215e56
Girnar247 <100025295+Girnar247@users.noreply.github.com>
2022-09-30 17:46:56 +0200

    falana
    extended falana dhimkana

---
+++ time_compute.py
@@ -0,0 +1,17 @@
+from tqdm import tqdm
+import time
+
+
+t1 = time.time()
+print(t1)
+
+# for i in range(10000000):
+#     a = i**2
+
+for i in tqdm(range(10000000)):
+    a = i**2
+    
+t2 = time.time()
+print(t2)
+
+print('Time diff: ', (t2-t1))
